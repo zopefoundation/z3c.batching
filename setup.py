e839f81fcdebd10d509fbbc4e371e09845ae0fe2
Nikolay Kim <fafhrd91@gmail.com>
2008-09-09 10:37:06 +0000

    vb

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='z3c.batching',
-      version = '1.0.1dev',
+      version = '1.1.0dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Batching',
