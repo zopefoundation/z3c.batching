f05a91be44189b705f7f9789755d8260efc1c3eb
Marius Gedminas <marius@gedmin.as>
2016-06-05 11:48:09 +0300

    Reformat setup.py to follow PEP-8

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@
 #
 ##############################################################################
 """Setup for z3c.batching package"""
+
 import os
 from setuptools import setup, find_packages
 
@@ -20,52 +21,55 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-setup(name='z3c.batching',
-      version='2.1.0.dev0',
-      author='Zope Corporation and Contributors',
-      author_email='zope-dev@zope.org',
-      description='List batching support',
-      long_description=(
-          read('README.txt')
-          + '\n\n'
-          + '======================\n'
-          + 'Detailed Documentation\n'
-          + '======================\n'
-          + '\n\n'
-          + read('src', 'z3c', 'batching', 'README.txt')
-          + '\n\n'
-          + read('src', 'z3c', 'batching', 'subset.txt')
-          + '\n\n'
-          + read('CHANGES.txt')),
-      keywords="zope3 batching",
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Zope Public License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Topic :: Internet :: WWW/HTTP',
-          'Framework :: Zope3'],
-      url='https://pypi.python.org/pypi/z3c.batching',
-      license='ZPL 2.1',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['z3c'],
-      install_requires=['setuptools',
-                          'zope.interface',
-                          'zope.schema',
-                          ],
-      include_package_data=True,
-      zip_safe=False,
-      )
+setup(
+    name='z3c.batching',
+    version='2.1.0.dev0',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    description='List batching support',
+    long_description=''.join([
+        read('README.txt'),
+        '\n\n',
+        '======================\n',
+        'Detailed Documentation\n',
+        '======================\n',
+        '\n\n',
+        read('src', 'z3c', 'batching', 'README.txt'),
+        '\n\n',
+        read('src', 'z3c', 'batching', 'subset.txt'),
+        '\n\n',
+        read('CHANGES.txt'),
+    ]),
+    keywords="zope3 batching",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='https://pypi.python.org/pypi/z3c.batching',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['z3c'],
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.schema',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
