05159a769e9b59ffa0e80e52a06cf9a0065bd586
Michael Howitz <mh@gocept.com>
2023-02-24 08:25:03 +0100

    Back to development: 3.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
 
 setup(
     name='z3c.batching',
-    version='3.0',
+    version='3.1.dev0',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.dev',
     description='List batching support',
