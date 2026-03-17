0a4000fdcd289b52a0538cb377918e6eac13210a
Michael Howitz <icemac@gmx.net>
2026-03-17 09:03:23 +0100

    Move package metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -12,66 +12,8 @@
 #
 ##############################################################################
 """Setup for z3c.batching package"""
-
-import os
-
 from setuptools import setup
 
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-
-setup(
-    name="z3c.batching",
-    version="4.1.dev0",
-    author="Zope Foundation and Contributors",
-    author_email="zope-dev@zope.dev",
-    description="List batching support",
-    long_description="".join(
-        [
-            read("README.rst"),
-            "\n\n",
-            "======================\n",
-            "Detailed Documentation\n",
-            "======================\n",
-            "\n\n",
-            read("src", "z3c", "batching", "README.rst"),
-            "\n\n",
-            read("src", "z3c", "batching", "subset.rst"),
-            "\n\n",
-            read("CHANGES.rst"),
-        ]
-    ),
-    keywords="zope3 batching",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Zope Public License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Topic :: Internet :: WWW/HTTP",
-        "Framework :: Zope",
-        "Framework :: Zope :: 3",
-    ],
-    url="https://github.com/zopefoundation/z3c.batching",
-    license="ZPL-2.1",
-    python_requires=">=3.10",
-    install_requires=[
-        "setuptools",
-        "zope.interface",
-        "zope.schema",
-    ],
-    include_package_data=True,
-    zip_safe=False,
-)
+# See pyproject.toml for package metadata
+setup()
